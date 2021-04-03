484bada8ee4df173ddb59a1bac4a4c1878c7c3af
nakamori-naoya <nmk623tk@gmail.com>
2021-04-04 00:11:06 +0900

    userテーブルの作成

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,12 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_03_063737) do
+ActiveRecord::Schema.define(version: 2021_03_13_043238) do
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "title", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+    t.string "password_digest"
+    t.string "email", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
