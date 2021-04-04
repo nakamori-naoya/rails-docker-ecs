4f243a3049e8a89a62fb513f01bf811144ae0215
nakamori-naoya <nmk623tk@gmail.com>
2021-04-04 17:21:59 +0900

    validationの記述
    テストはこれから書く。butオブジェクト指向設計の本を読むかもしれません

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+    VALID_PASSWORD_REGEX =/\A(?=.*?[a-z])(?=.*?[\d])\w{8,12}\z/
+    #英小文字と数字で8~12文字
+    validates :email, presense: true, format: { with: VALID_EMAIL_REGEX }
+    validates :password_digest, presense: true , format: { with: VALID_PASSWORD_REGEX }
 end
