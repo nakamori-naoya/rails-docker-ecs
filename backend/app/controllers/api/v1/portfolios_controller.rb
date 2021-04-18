0236bd75d30a05c787317b23a438d4d31831053d
nakamori-naoya <nmk623tk@gmail.com>
2021-04-18 17:09:06 +0900

    Detail画面の一応の実装完了

--- backend/app/controllers/api/v1/portfolios_controller.rb
+++ backend/app/controllers/api/v1/portfolios_controller.rb
@@ -2,22 +2,28 @@ class Api::V1::PortfoliosController < ApplicationController
     before_action :authenticate_user, except: [:index]
 
     def index
+        #究極的に検索にもmatched_portfolios()のロジックは使えますね！！
         @new_arrival = Portfolio.limit(10).order("created_at DESC").includes(:user)
-        @highly_creativity = matched_portfolios(AvgEval.search_columns(10, "creativity", "portfolio_id".intern))
-        @highly_sociality = matched_portfolios(AvgEval.search_columns(10, "sociality", "portfolio_id".intern))
-        @highly_skill = matched_portfolios(AvgEval.search_columns(10, "skill", "portfolio_id".intern))
-        @highly_usability = matched_portfolios(AvgEval.search_columns(10, "usability", "portfolio_id".intern))
-        @highly_business_oriented = matched_portfolios(AvgEval.search_columns(10, "business_oriented", "portfolio_id".intern))
-        @highly_comprehensive_evaluation = matched_portfolios(AvgEval.search_columns(10, "comprehensive_evaluation", "portfolio_id".intern))
+        @high_creativity = matched_portfolios(AvgEval.search_columns(10, "creativity", "portfolio_id".intern))
+        @high_sociality = matched_portfolios(AvgEval.search_columns(10, "sociality", "portfolio_id".intern))
+        @high_skill = matched_portfolios(AvgEval.search_columns(10, "skill", "portfolio_id".intern))
+        @high_usability = matched_portfolios(AvgEval.search_columns(10, "usability", "portfolio_id".intern))
+        @high_business_oriented = matched_portfolios(AvgEval.search_columns(10, "business_oriented", "portfolio_id".intern))
+        @high_comprehensive_evaluation = matched_portfolios(AvgEval.search_columns(10, "comprehensive_evaluation", "portfolio_id".intern))
         render json: {status: 200, 
-                      new_arrival: @new_arrival, 
-                      highly_creativity: @highly_creativity,
-                      highly_sociality:  @highly_sociality,
-                      highly_skill: @highly_skill,
-                      highly_usability: @highly_usability,
-                      highly_business_oriented: @highly_business_oriented,
-                      highly_comprehensive_evaluation: @highly_comprehensive_evaluation
+                      newArrival: @new_arrival, 
+                      highCreativity: @high_creativity,
+                      highSociality:  @high_sociality,
+                      highSkill: @high_skill,
+                      highUsability: @high_usability,
+                      highBusinessOriented: @high_business_oriented,
+                      highComprehensiveEvaluation: @high_comprehensive_evaluation
                     }
+    end
+    
+
+    def search
+        #引数で送られてきた数字と評価項目のポートフォリオを返す関数にしたい
     end
 
     def matched_portfolios(ids)
