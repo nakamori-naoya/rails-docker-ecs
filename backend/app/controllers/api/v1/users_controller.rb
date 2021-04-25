ce616ae59ea916fce15d4505fa75713f3c3746bf
nakamori-naoya <nmk623tk@gmail.com>
2021-04-25 19:51:43 +0900

    ユーザー管理系の実装完了

--- backend/app/controllers/api/v1/users_controller.rb
+++ backend/app/controllers/api/v1/users_controller.rb
@@ -1,15 +1,10 @@
 class Api::V1::UsersController < ApplicationController
-    # before_action :authenticate_user, except: [:create, :logout]
+    before_action :authenticate_user, except: [:create, :logout]
 # curl -X "GET" "http://localhost:3000/api/v1/users" -H "Authorization: Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE2MTU2OTc4MTcsInN1YiI6MX0.HMjFKqWzMJW8Jy28aZWpjldHaMPsknWjZAh7UuX4rY0" -H "Content-Type: application/json"
 
     def index
-        if current_user?
-          @image = current_user.image.attached? ? user.attributes.merge({image: url_for(current_user.image)}) :user
-          render json: {status: 200, user: current_user, image: @image}
-        else
-          response_not_found("user")
-        end
-        
+          @user = current_user.image.attached? ? current_user.attributes.merge({image: url_for(current_user.image)}) :current_user
+          render json: {status: 200, data: response_fields(@user.to_json)}
     end
 
 
@@ -19,7 +14,7 @@ class Api::V1::UsersController < ApplicationController
           response_bad_request
         else
             if @user.save
-              render json: {status: 201, data: current_user }
+              render json: {status: 201, data: @user }
             else
               response_internal_server_error
             end
@@ -36,18 +31,20 @@ class Api::V1::UsersController < ApplicationController
       end
 
       def update
-          user = User.find(params[:id])
-          user.image.attach(params[:image])
-          user.update(users_params)
-
-          user=  User.find(params[:id])
-          @image = user.image.attached? ? user.attributes.merge({image: url_for(user.image)}) :user
-          render json: {status: 200, data: @image}
+          previous_user = User.find(params[:id]) 
+          previous_user.update(users_params)
+          @user = current_user.image.attached? ? current_user.attributes.merge({image: url_for(current_user.image)}) :current_user
+          render json: {status: 200, data: response_fields(@user.to_json)}
       end
 
       private
       def users_params
-        params.permit(:email, :password, :nickName, :image)
+        params.permit(:id, :email, :password, :nickName, :image)
+      end
+
+      def response_fields(user_json)
+        user_parse = JSON.parse(user_json)
+        user_parse.except('created_at', 'updated_at', 'email' ,'password_digest')
       end
 
 end
